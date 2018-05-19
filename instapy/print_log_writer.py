7f75f82ca41d7b34738dd74ff4b73c9a3b15ecac
Sha <mohansha@github.com>
2018-05-19 17:39:13 +0530

    added print logging

---
+++ instapy/print_log_writer.py
@@ -0,0 +1,28 @@
+"""Module only used to log the number of followers to a file"""
+from datetime import datetime
+
+
+def log_follower_num(browser, username, logfolder):
+    """Prints and logs the current number of followers to
+    a seperate file"""
+    browser.get('https://www.instagram.com/' + username)
+
+    followed_by = browser.execute_script(
+        "return window._sharedData.""entry_data.ProfilePage[0]."
+        "user.followed_by.count")
+
+    with open('{}followerNum.txt'.format(logfolder), 'a') as numFile:
+        numFile.write(
+            '{:%Y-%m-%d %H:%M} {}\n'.format(datetime.now(), followed_by or 0))
+
+    return followed_by
+
+
+def log_followed_pool(login, followed, logger, logfolder):
+    """Prints and logs the followed to
+    a seperate file"""
+    try:
+        with open('{0}{1}_followedPool.csv'.format(logfolder, login), 'a+') as followPool:
+            followPool.write(followed + ",\n")
+    except BaseException as e:
+        logger.error("log_followed_pool error {}".format(str(e)))
