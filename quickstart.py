f6db708ab52273310ee63565fa261fab39f4eb59
Sha <mohansha@github.com>
2018-05-19 17:33:08 +0530

    added quickstart

---
+++ quickstart.py
@@ -0,0 +1,30 @@
+from instapy import InstaPy
+
+insta_username = ''
+insta_password = ''
+
+# set headless_browser=True if you want to run InstaPy on a server
+try:
+    # set these if you're locating the library in the /usr/lib/pythonX.X/ directory
+    # Settings.database_location = '/path/to/instapy.db'
+    # Settings.browser_location = '/path/to/chromedriver'
+    
+    session = InstaPy(username=insta_username,
+                      password=insta_password,
+                      headless_browser=False,
+                      multi_logs=True)
+    session.login()
+
+    # settings
+    #session.set_upper_follower_count(limit=2500)
+    #session.set_do_comment(True, percentage=10)
+    #session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
+    #session.set_dont_include(['friend1', 'friend2', 'friend3'])
+    session.set_dont_like(['pizza', 'girl'])
+
+    # actions
+    session.like_by_tags(['chennai'], amount=100)
+
+finally:
+    # end the bot session
+    session.end()
